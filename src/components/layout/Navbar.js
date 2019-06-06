c4dcf8be3ed81a01a81e3efa0b07761909edcbe6
JAJI <jajmantobi@gmail.com>
2019-06-06 23:24:16 +0100

    GTS-1 #comment create Users component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  static defaultProps = {
+    title: "Github Default",
+    icon: "fab fa-github"
+  };
+
+  // static propTypes = {
+  //   title: PropTypes.string.isRequired,
+  //   icon: PropTypes.string.isRequired
+  // };
+
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <h1>
+          <i class="this.props.icon" /> {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
