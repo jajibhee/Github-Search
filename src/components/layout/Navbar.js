f155024f79fd5a7d01bb0a1ae6665346e9c78c53
JAJI <jajmantobi@gmail.com>
2019-06-11 23:30:39 +0100

    search bar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Default",
-    icon: "fab fa-github"
-  };
+// export class Navbar extends Component {
+//   static defaultProps = {
+//     title: "Github Default",
+//     icon: "fab fa-github"
+//   };
 
-  // static propTypes = {
-  //   title: PropTypes.string.isRequired,
-  //   icon: PropTypes.string.isRequired
-  // };
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     icon: PropTypes.string.isRequired
+//   };
 
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i class="this.props.icon" /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <nav className="navbar bg-primary">
+//         <h1>
+//           <i class="this.props.icon" /> {this.props.title}
+//         </h1>
+//       </nav>
+//     );
+//   }
+// }
 
+// export default Navbar;
+
+//Functonal component , destructure props.icon and props.title
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Default",
+  icon: "fab fa-github"
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class UserItem extends Component {
-  // state = {
-  //   login: "trevor",
-  //   id: 5945,
-  //   avatar_url: "https://avatars0.githubusercontent.com/u/5945?v=4",
-  //   html_url: "https://github.com/trevor"
-  // };
-
   render() {
-    //destructure the state keys
+    //destructure the state keys in user.js
     const { login, id, avatar_url, html_url } = this.props.user;
 
     return (
@@ -32,3 +26,30 @@ class UserItem extends Component {
 }
 
 export default UserItem;
+
+//Refactoring to Functional components
+// const UserItem = ({user: { login, id, avatar_url, html_url } }) => {
+
+//   return (
+//     <div className="card text-center">
+//       <img
+//         src={avatar_url}
+//         alt=""
+//         className="round-img"
+//         style={{ width: "60px" }}
+//       />
+//       <h3>{login}</h3>
+//       <div>
+//         <a href={html_url} className="btn btn-dark btn-sm my-1">
+//           More
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+// export default UserItem;
